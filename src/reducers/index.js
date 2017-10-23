863b0feae5788129760c0cf18470bd0b478af5cf
Medimuni <472731@cognizant.com>
2017-10-23 13:32:07 +0530

    base code added

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { routerReducer } from 'react-router-redux';
+import { combineReducers } from 'redux';
+
+
+const root = combineReducers({
+  router: routerReducer
+});
+
+export default root;
